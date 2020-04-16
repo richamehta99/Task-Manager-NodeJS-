b2647ef403a886ebe66fd797b6ae1e4248a3a814
Richa Mehta <richa.mehta@nagarro.com>
2020-04-16 13:55:09 +0530

    updating the server

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const todoRoute = require('./routes/todos')
 
 const SERVER_PORT= process.env.PORT || 7070
 const app = express()
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
  
 app.use('/', express.static(__dirname + '/public'))
  
