360b930cb8f7894315ddadcf32860549343e6933
Richa Mehta <richa.mehta@nagarro.com>
2020-04-16 11:59:39 +0530

    adding port from env if available

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const express = require('express')
 
 const { db } = require('./task-notes')
 const todoRoute = require('./routes/todos')
- 
+
+const SERVER_PORT= process.env.PORT || 7070
 const app = express()
  
 app.use(express.urlencoded({ extended: true }))
@@ -14,7 +15,7 @@ app.use('/todos', todoRoute)
  
 db.sync()
   .then(() => {
-    app.listen(7070)
+    app.listen(SERVER_PORT)
   })
   .catch((err) => {
     console.error(err)
